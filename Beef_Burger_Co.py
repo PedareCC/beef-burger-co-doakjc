c6fc128420fdccbf2a0e2c210fcdd383d1e14c8e
doakjc <165220132+doakjc@users.noreply.github.com>
2025-09-01 05:42:43 +0000

    1/9 - Adds comments on current code

--- Beef_Burger_Co.py
+++ Beef_Burger_Co.py
@@ -1,7 +1,8 @@
-menu = {'Burgers': {
-    'Cheeseburger': {
-    'Price': 8.99,
-    'Toppings': ['Beef Patty', 'Cheese', 'Pickles', 'Onion', 'Ketchup', 'Mustard']
+#Full Menu Dictionary
+menu = {'Burgers': { #Burger Menu Dictionary
+    'Cheeseburger': { #Dictionary for individual item
+    'Price': 8.99, #Key:Value pair for price
+    'Toppings': ['Beef Patty', 'Cheese', 'Pickles', 'Onion', 'Ketchup', 'Mustard'] #Key:Value Pair with list for toppings 
     },
     'Beef Burger': {
         'Price': 9.95,
@@ -12,56 +13,57 @@ menu = {'Burgers': {
         'Toppings': ['Beef Patty', 'Cheese', 'Bacon', 'Onion Rings', 'Barbecque Sauce']
     }
     },
-    'Sides': {'Small Fries':{
+    'Sides': { #Sides Menu Dictionary
+        'Small Fries':{ 
         'Price':3.49
     }},
-    'Drinks': {'Small Coke':{'Price': 3.49},
-               'Large Coke':{'Price':4.99}}
+    'Drinks': { #Drinks Menu Dictionary
+        'Small Coke':{'Price': 3.49},
+        'Large Coke':{'Price':4.99}}
 }
 
+#Toppings that a customer can add to their burger
 burger_toppings = ['Beef Patty', 'Bacon', 'Cheese', 'Pickles', 'Onion', 'Onion Rings', 'Tomato', 'Lettuce', 'Ketchup', 'Mustard', 'Barbecque Sauce']
 
-customer_order = {'Burgers':{
+#Customer order Dictionary, information gets added to this after an item is ordered, and this is then printed out at the checkout
+customer_order = {'Burgers':{ #Dictionary for Burger Orders
 
 },
-                  'Sides':'',
-                  'Drinks':''}
+                  'Sides':{}, #Dictionary for Sides Orders
+                  'Drinks':{}} #Dictionary for Drinks Orders
 
-def order():
+def order(): #Function for ordering a burger
     print("Welcome to Beef Burger Co!")
     while True:
         print("Here's our menu:")
-        for index, key in enumerate(menu.keys()):
+        for index, key in enumerate(menu.keys()): #Prints out keys of menu dictionary (names of sub-menus) - enumerate() allows these to be printed as a numerical list
             print(f"{index+1}. {key}")
-        try:
-            choice = int(input("Enter 1, 2, or 3 to select a menu to view, or enter 4 to complete your order: "))
-            if choice == 1:
-                burgers()
-            elif choice == 2:
-                sides()
-            elif choice == 3:
-                drinks()
-            elif choice == 4:
-                checkout()
-            else:
-                print("Invalid choice.")
-        except ValueError:
-            print('Invalid Choice')
+        choice = input("Enter 1, 2, or 3 to select a menu to view, or enter 4 to complete your order: ") #Customer selects menu or proceeds to checkout
+        if choice == 1:
+            burgers() #Goes to burger menu
+        elif choice == 2:
+            sides() #Goes to sides menu
+        elif choice == 3:
+            drinks() #Goes to drinks menu
+        elif choice == 4:
+            checkout() #Goes to checkout
+        else:
+            print("Invalid choice.") #If customer enters a value other than 1, 2, 3 or 4
 
-def burgers():
+def burgers(): #Burger Menu
     print('burger')
 
 
-def sides():
+def sides(): #Sides Menu
     print('sides')
 
-def drinks():
+def drinks(): #Drinks Menu
     print('drinks')
 
-def checkout():
+def checkout(): #Checkout
     print('money please')
     #Print out order and cost
-    quit()
+    quit() #Ends program
 
 
-order()
+order() #Calls function to begin program
