4b20eb96578c1b0a6a9daf2f6ae33327faf31f33
doakjc <165220132+doakjc@users.noreply.github.com>
2025-09-01 05:29:43 +0000

    1/9 - First Commit - Adds Basic Menu Dictionary - Adds Burger Toppings List - Adds Customer Order Dictionary - Order function - Placeholders for other functions

--- Beef_Burger_Co.py
+++ Beef_Burger_Co.py
@@ -1,31 +1,65 @@
-beef_burgers = {
-    'Cheeseburger': 4.99,
+menu = {'Burgers': {
+    'Cheeseburger': {
+    'Price': 8.99,
+    'Toppings': ['Beef Patty', 'Cheese', 'Pickles', 'Onion', 'Ketchup', 'Mustard']
+    },
+    'Beef Burger': {
+        'Price': 9.95,
+        'Toppings': ['Beef Patty', 'Cheese', 'Tomato', 'Lettuce', 'Pickles', 'Ketchup', 'Mustard']
+    },
+     'Bacon Burger': {
+        'Price': 9.95,
+        'Toppings': ['Beef Patty', 'Cheese', 'Bacon', 'Onion Rings', 'Barbecque Sauce']
+    }
+    },
+    'Sides': {'Small Fries':{
+        'Price':3.49
+    }},
+    'Drinks': {'Small Coke':{'Price': 3.49},
+               'Large Coke':{'Price':4.99}}
 }
 
-total_order = []          
-total_order_prices = []   
+burger_toppings = ['Beef Patty', 'Bacon', 'Cheese', 'Pickles', 'Onion', 'Onion Rings', 'Tomato', 'Lettuce', 'Ketchup', 'Mustard', 'Barbecque Sauce']
+
+customer_order = {'Burgers':{
+
+},
+                  'Sides':'',
+                  'Drinks':''}
+
+def order():
+    print("Welcome to Beef Burger Co!")
+    while True:
+        print("Here's our menu:")
+        for index, key in enumerate(menu.keys()):
+            print(f"{index+1}. {key}")
+        try:
+            choice = int(input("Enter 1, 2, or 3 to select a menu to view, or enter 4 to complete your order: "))
+            if choice == 1:
+                burgers()
+            elif choice == 2:
+                sides()
+            elif choice == 3:
+                drinks()
+            elif choice == 4:
+                checkout()
+            else:
+                print("Invalid choice.")
+        except ValueError:
+            print('Invalid Choice')
+
+def burgers():
+    print('burger')
 
 
-def menu():
-    print('--- Beef Burger Menu ---')
-    print(f"1. Cheeseburger for ${beef_burgers['Cheeseburger']}")
-    print('2. Go to checkout.')
+def sides():
+    print('sides')
+
+def drinks():
+    print('drinks')
+
+def checkout():
+    print('money please')
 
 
-def customer_order():
-        order = input('Select what you would like to order: ')
-  
-        if order == 1:
-
-        elif
-
-        else 
-
-def check_out():
-    print('\n--- Your Order Summary ---')
-
-
-print('Welcome to the Beef Burger Co. menu!')
-menu()
-customer_order()
-check_out()
+order()
