664b248d5c03f5183df689f7bb7c8c1bb038eb6c
doakjc <165220132+doakjc@users.noreply.github.com>
2025-09-06 04:24:21 +0000

    6/9 - Updates checkout formatting

--- Beef_Burger_Co.py
+++ Beef_Burger_Co.py
@@ -114,9 +114,9 @@ def burgers(): #Burger Menu
                                             if choice > 0:
                                                 print(f"{choice} {selected_burger} added to order") 
                                                 if updated_toppings != details['Toppings']:
-                                                    customer_order['Burgers'].append({'Burger': selected_burger, 'Quantity':choice,'Price': details['Price'], 'Additions':additions, 'Removals':removals, 'Updated Toppings': updated_toppings}) #Adds order details to customer_orders dictionary to print out at checkout
+                                                    customer_order['Burgers'].append({'Item': selected_burger, 'Quantity':choice,'Price': details['Price'], 'Additions':additions, 'Removals':removals, 'Updated Toppings': updated_toppings}) #Adds order details to customer_orders dictionary to print out at checkout
                                                 else:
-                                                    customer_order['Burgers'].append({'Burger': selected_burger, 'Quantity':choice,'Price': details['Price'], 'Additions':additions, 'Removals':removals}) #Adds order details to customer_orders dictionary to print out at checkout 
+                                                    customer_order['Burgers'].append({'Item': selected_burger, 'Quantity':choice,'Price': details['Price'], 'Additions':additions, 'Removals':removals}) #Adds order details to customer_orders dictionary to print out at checkout 
                                                 break
                                                 
                                             else:
@@ -132,7 +132,7 @@ def burgers(): #Burger Menu
                                     choice = int(input('How many would you like to order? '))
                                     if choice > 0:
                                         print(f"{choice} {selected_burger} added to order")
-                                        customer_order['Burgers'].append({'Burger': selected_burger, 'Quantity':choice,'Price': details['Price'], 'Additions':'', 'Removals':''}) #Adds order details to customer_orders dictionary to print out at checkout
+                                        customer_order['Burgers'].append({'Item': selected_burger, 'Quantity':choice,'Price': details['Price'], 'Additions':'', 'Removals':''}) #Adds order details to customer_orders dictionary to print out at checkout
                                         break
                                     else:
                                         print("Enter a valid input")
@@ -164,11 +164,14 @@ def checkout(): #Checkout
     if len(customer_order['Burgers']) != 0: #Checks if no burgers were ordered
         print('Burgers')
         for burger in customer_order['Burgers']:
-            for menu, order in burger.items():
-                if menu == 'Price':
-                    print(f"Price: ${order}") #Different print to add dollar sign 
+            for key, value in burger.items():
+                if key == 'Additions' or key == 'Removals':
+                    if len(value) > 0:
+                        print(f'{key}: {value}')
+                elif key == 'Price':
+                    print(f"Price: ${value}") #Different print to add dollar sign 
                 else:
-                    print(menu, order)
+                    print(f'{key}: {value}')
     sys.exit() #Ends program
 
 
